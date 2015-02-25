46ec26279a35ef254009947f599725351d268009
Malte-Maurice Dreyer <dreyer.maltem+dev@gmail.com>
2015-02-25 21:24:49 +0000

    Describe #toString() and parts separately

--- test/no-args.js
+++ test/no-args.js
@@ -10,46 +10,50 @@ describe("Ipv4Address()", function() {
         addr = new Ipv4Address();
     });
 
-    it("should be initialised with 0 for each part", function() {
-        assert.equal(addr[0], 0);
-        assert.equal(addr[1], 0);
-        assert.equal(addr[2], 0);
-        assert.equal(addr[3], 0);
+    describe("parts", function() {
+        it("should be initialised with 0 for each part", function() {
+            assert.equal(addr[0], 0);
+            assert.equal(addr[1], 0);
+            assert.equal(addr[2], 0);
+            assert.equal(addr[3], 0);
+        });
+
+        it("should allow to modify parts", function() {
+            let arr = new Array(4).fill(0);
+
+            for(let i = 0; i < 4; ++i) {
+                addr[i] = arr[i] = i + 1;
+                assert.deepEqual(addr, arr);
+            }
+            assert.deepEqual(addr, [1,2,3,4]);
+        });
+
+        it("should cut off numbers when to high (like Uint8Array)", function() {
+            let arr = new Array(4).fill(0);
+
+            for(let i = 0; i < 4; ++i) {
+                arr[i] = i + 1;
+                addr[i] = arr[i] + 255 + 1;
+                assert.deepEqual(addr, arr);
+            }
+            assert.deepEqual(addr, [1,2,3,4]);
+        });
     });
 
-    it("should be 0.0.0.0 when cast to string", function() {
-        assert.equal(String(addr), "0.0.0.0");
-    });
+    describe("#toString()", function() {
+        it("should be 0.0.0.0 when not modified", function() {
+            assert.equal(addr.toString(), "0.0.0.0");
+        });
 
-    it("should allow to modify parts", function() {
-        let arr = new Array(4).fill(0);
+        it("should work when parts are modified", function() {
+            let arr = new Array(4).fill(0);
 
-        for(let i = 0; i < 4; ++i) {
-            addr[i] = arr[i] = i + 1;
-            assert.deepEqual(addr, arr);
-        }
-        assert.deepEqual(addr, [1,2,3,4]);
-    });
-
-    it("should correctly stringify when parts are modified", function() {
-        let arr = new Array(4).fill(0);
-
-        for(let i = 0; i < 4; ++i) {
-            addr[i] = arr[i] = i + 1;
-            assert.equal(String(addr), arr.join("."));
-        }
-        assert.equal(String(addr), "1.2.3.4");
-    });
-
-    it("should cut off numbers when to high (like Uint8Array)", function() {
-        let arr = new Array(4).fill(0);
-
-        for(let i = 0; i < 4; ++i) {
-            arr[i] = i + 1;
-            addr[i] = arr[i] + 255 + 1;
-            assert.deepEqual(addr, arr);
-        }
-        assert.deepEqual(addr, [1,2,3,4]);
+            for(let i = 0; i < 4; ++i) {
+                addr[i] = arr[i] = i + 1;
+                assert.equal(String(addr), arr.join("."));
+            }
+            assert.equal(String(addr), "1.2.3.4");
+        });
     });
 
     describe("#keys()", function() {
